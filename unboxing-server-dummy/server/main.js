0fa897798a530b3f3bad7249946c69157e5b7991
Holger H <holgheiss@gmail.com>
2018-02-21 13:36:06 +0100

    add meteor dummy server. move client directory

---
+++ unboxing-server-dummy/server/main.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  /*
+  for (var i = 1; i<10; i++) {
+  	Samples.insert({
+  		_id: "id"+i,
+  		name: "name "+i,
+  		url: "url "+i
+  	})	
+  }*/
+  
+});
