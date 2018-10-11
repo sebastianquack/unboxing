0de2a88a97f95ebf6ef848e5ed60ffe33e1b4c35
Holger H <holgheiss@gmail.com>
2018-10-11 11:46:57 +0200

    registerReactiveStateCallback

--- unboxing-app/app/services/Service.js
+++ unboxing-app/app/services/Service.js
@@ -6,6 +6,7 @@ export default class Service {
 		this.serviceName = name;
 		this.state = {};
 		this.initialState = initialState;
+		this.reactiveUpadateCallbacks = [];
 	}
 
 	registerOnChange(func) {
@@ -18,8 +19,23 @@ export default class Service {
 		Object.keys(newState).forEach((key) => {
 			this.state[key] = newState[key];
 		});
+		// send to react
 		this.onChange(this.serviceName, this.state);
+		// send to other services
+		for (let handle in this.reactiveUpadateCallbacks) {
+			this.reactiveUpadateCallbacks[handle](this.state)
+		}
 	}
+
+	// register a callback for reactive state updates
+	registerReactiveStateCallback = (func, handle) => {
+		this.reactiveUpadateCallbacks[handle] = func
+		func(this.state) // initial call
+	}
+
+	unRegisterReactiveStateCallback = (handle) => {
+		delete this.reactiveUpadateCallbacks[handle]
+	}		
 
 	getReactive(key) {
 		return this.state[key];
