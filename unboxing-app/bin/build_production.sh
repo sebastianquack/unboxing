faed1f06ba9ce9bfb3aa3714571b9becf7b018e1
Sebastian Quack <sebquack@Sebastians-MacBook-Pro.local>
2019-03-14 15:03:00 +0100

    add build and deploy script for tablets

---
+++ unboxing-app/bin/build_production.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## make production build
+cd android && ./gradlew assembleRelease
---
+++ unboxing-app/bin/deploy_production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+## declare an array variable
+declare -a deviceIds=("03")
+
+## You can access them using "${deviceIds[0]}", "${deviceIds[1]}" 
+
+cd android
+
+## loop over deviceIds
+for i in "${deviceIds[@]}"
+do
+   echo "connecting to device ${i}"
+   adb disconnect
+   adb connect "192.168.8.1${i}:5555"
+
+   echo "uninstalling app"
+   adb uninstall com.unboxing
+
+   echo "installing app"
+   adb install "./app/build/outputs/apk/release/app-release.apk"
+   
+   echo "starting app"
+   adb shell am start -n com.unboxing/com.unboxing.MainActivity
+
+done
+
+cd ..
