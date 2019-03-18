9e143ac82a6c950e371b3181a84d4b30878def56
Holger H <holgheiss@gmail.com>
2019-03-18 22:41:06 +0100

    improve files download

--- unboxing-app/app/components/admin/Files.js
+++ unboxing-app/app/components/admin/Files.js
@@ -17,18 +17,45 @@ class Files extends React.Component {
     this.state = {
       localFiles : {}
     };
+    this.fileStaus = this.fileStatus.bind(this)
+    this.updateFileInfo = this.updateFileInfo.bind(this)
+    this.downloadFile = this.downloadFile.bind(this)
+    this.renderFile = this.renderFile.bind(this)
   }
 
-  updateFileInfo = file => {
-    console.log(file)
+  fileStatus(fileInfo) {
+    const files = this.props.storageService.collections.files
+    const file = files.find(file => file._id === fileInfo._id)
+    if (file && fileInfo.exists && fileInfo.size === file.size) {
+      return "OK"
+    } else {
+      if (fileInfo.size && file.size) {
+        return Math.round(100*fileInfo.size/file.size)+ "%"
+      } else {
+        return "-"
+      }
+    }
+  }
 
-    let info = this.state.localFiles[file._id] || {}
+  updateFilesize(file_id, size) {
+    this.setState(
+      { 
+        localFiles: { 
+          ...this.state.localFiles, 
+          [file_id]: {
+            ...this.state.localFiles[file_id],
+            size
+          } 
+        } 
+      })
+    }
 
-    if (info && info.exists && !info.downloading) {
-      return
-    }    
+  updateFileInfo(file) {
+    let info = this.state.localFiles[file._id] || {
+      _id: file._id
+    }
 
-    RNFS.exists(folder + file.path)
+    return RNFS.exists(folder + file.path)
       .then( exists => {
         if (exists) {
           info.exists = true
@@ -44,93 +71,55 @@ class Files extends React.Component {
         //console.log("stat", stat)
         info.size = stat ? stat.size : 0
       })
-      .then( () => {
-
-        let start = false
-        console.log("DOWN?", file.path, info, file.size, info.size)
-        if (!info.downloading && info.size != file.size) {
-          start = true
-        } else if (info.size == file.size) {
-          info.downloading = false
-        }
-
-        this.setState(prevState => {
-          let state = prevState
-          state.localFiles[file._id] = info
-          return state
-        }, () => {
-          if (start) {
-            console.log("Object.entries(this.state.localFiles)", Object.entries(this.state.localFiles))
-            const firstDownloadingFile = Object.entries(this.state.localFiles).find( e => e[1].downloading )
-            console.log("firstDownloadingFile", firstDownloadingFile)
-            if (!firstDownloadingFile) {
-
-              console.log("starting download", file.path)
-
-              this.setState(state => {
-                state.localFiles[file._id].downloading = true
-                return state
-              })
-
-              RNFS.downloadFile({
-                fromUrl: 'http://'+this.props.networkService.server+':3000' + file.url_path,
-                toFile: folder + file.path,
-                progress: p => {this.setState((prevState) => { 
-                  prevState.localFiles[file._id].size = p.bytesWritten; 
-                  return prevState 
-                },()=>{
-                }
-              )}
-              }).promise.then( (jobId, more) => {
-                console.log(jobId, more)
-                this.updateFilesInfo()
-                setTimeout(this.updateFilesInfo,1000)
-              }).catch((err) => {
-                console.log(err.message, err.code);
-              });       
-
-            } else {
-              console.log("not yet downloading ", file.path)
-            }
-          }   
-        })
-        
-
-      })
       .catch((err) => {
         console.log(err.message, err.code);
-      });   
- }
+      })
+      .finally( () => {
+        // console.log("new info", info)
+        this.setState({ localFiles: { ...this.state.localFiles, [file._id]: info } })
+      })   
+  }
 
-  renderFiles = () => {
-    if (this.props.storageService.collections.files) {
-      return this.props.storageService.collections.files.map( this.renderFile )
+  downloadFile(file) {
+    //console.log(file)
+
+    let info = this.state.localFiles[file._id] || {
+      _id: file._id
     }
+
+    return RNFS.downloadFile(
+      {
+        fromUrl: 'http://'+this.props.networkService.server+':3000' + file.url_path,
+        toFile: folder + file.path,
+        progressDivider: 5,
+        cacheable: false,
+        progress: p => {this.updateFilesize(file._id, p.bytesWritten)}
+      }
+    ).promise.then( result => {
+      console.log("download completed with code " + result.statusCode)
+      this.updateFilesize(file._id, result.bytesWritten)
+    }).catch((err) => {
+      console.log(err.message, err.code);
+    });       
   }
 
-  renderFile = (file) => {
-    const info = this.state.localFiles[file._id]
-    //console.log(info, file.path, this.state)
 
-    const perc = info && info.size ? Math.round(100 * info.size / file.size) : "?"
-    const finished = info && info.size && info.size == file.size && info.size > 0
-
-    return (<Text key={file._id}>
-      <Text>
-        { finished ? 
-          " DONE "
-          : perc+"%"
-        }
-      </Text>
-      {" "}
-      {file.path} 
-      </Text>)
-  }
-
-  updateFilesInfo = () => {
+  updateFilesInfo = async () => {
     for (let file of this.props.storageService.collections.files) {
-      this.updateFileInfo(file)
-    }    
+      console.log("checking " + file.path)
+      await this.updateFileInfo(file)
+    }
+    this.downloadFiles()
+  }
+
+  downloadFiles = async (file) => {
+    for (let file of this.props.storageService.collections.files) {
+      if (this.fileStatus(this.state.localFiles[file._id])  !== "OK") {
+        console.log("downloading " + file.path)
+        await this.downloadFile(file)
+        this.updateFileInfo(file)
+      }
+    }
   }
 
   handleUpdate = () => {
@@ -164,6 +153,28 @@ class Files extends React.Component {
     if (!prevProps.ready && this.props.ready) {
       // this.updateFilesInfo()
     }
+  }
+
+  renderFiles = () => {
+    if (this.props.storageService.collections.files) {
+      return this.props.storageService.collections.files.map( this.renderFile )
+    }
+  }
+
+  renderFile(file) {
+    const info = this.state.localFiles[file._id]
+    //console.log(info, file.path, this.state)
+
+    const status = info ? this.fileStatus(info) : "?"
+
+    return (<View key={file._id} style={{flexDirection:"row"}}>
+        <Text style={{width: 40}}>
+          { status }
+        </Text>
+        <Text>{" "}
+          {file.path} 
+        </Text>
+      </View>)
   }
 
   render() {
