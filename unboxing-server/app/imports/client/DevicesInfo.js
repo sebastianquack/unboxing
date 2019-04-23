a44e87829a30345a6966d81782f1d64ef30e27a5
Holger H <holgheiss@gmail.com>
2019-04-23 14:04:16 +0200

    admin server

--- unboxing-server/app/imports/client/DevicesInfo.js
+++ unboxing-server/app/imports/client/DevicesInfo.js
@@ -9,13 +9,6 @@ class DevicesInfo extends React.Component {
     super(props);
   }
 
-  columns = [
-  	'deviceId',
-  	'everythingVersion',
-  	'fileStatus',
-  	'timeSyncStatus',
-   ]
-
   DevicesInfoCss = css`
     ul {
       display: flex;
@@ -29,15 +22,29 @@ class DevicesInfo extends React.Component {
 
   render() {
 
-    const rows = this.props.devices.map(d => <tr>
-      { this.columns.map(c => <td>{d[c] || '-'}</td>) }
+    const columnAccessors = {
+      'deviceId': row => row.deviceId,
+      'connected': row => row.connected,
+      'everythingVersion': row => row.deviceStatus.everythingVersion,
+      'fileStatus': row => row.deviceStatus.fileStatus,
+      'timeSyncStatus': row => row.deviceStatus.timeSyncStatus,
+    }
+
+    const columnNames = Object.keys(columnAccessors)
+
+    const rows = this.props.devices.map(device => <tr key={device._id}>
+      { Object.entries(columnAccessors).map(
+        ([key, accessor]) => <td key={device._id+key}>{accessor(device) || '-'}</td>
+        )}
     </tr>)
 
     return (
       <div  className={this.DevicesInfoCss}>
         <table>
           <thead>
-            { this.columns.map(c => <th>{c}</th>) }
+            <tr>
+              { columnNames.map(c => <th key={c}>{c}</th>) }
+            </tr>
           </thead>
           <tbody>
             { rows }
--- unboxing-server/app/imports/startup/server/devices.js
+++ unboxing-server/app/imports/startup/server/devices.js
@@ -1,4 +1,5 @@
 express = require('express');
+import { Devices } from '../../collections/devices'
 
 const app = express();
 server = app.listen(3004);
@@ -9,15 +10,22 @@ function init(io) {
 
   // setup socket api
   io.on('connection', function(socket) {
-    console.log('Client connected: ' + socket.deviceId);
+    console.log('Client connected');
     
     socket.on('disconnect', () => {
       console.log('Client disconnected: ' + socket.deviceId);
-      
+      Devices.update({ deviceId }, {$set:{connected: false}})
     });
     
     socket.on('message', async function(msg) {
       console.log('Message received: ', msg);
+      socket.deviceId = msg.deviceId // store deviceId
+      Devices.update({ deviceId }, {$set:{connected: true}})
+
+      if (msg.code == "statusUpdate") {
+        Devices.update({ deviceId }, {$set:{deviceStatus: payload}})
+      }
+
     });
   });
 }
