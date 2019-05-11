7aee7ce168c7b324099aca20e2ac1560c8a5f873
Holger H <holgheiss@gmail.com>
2019-05-11 13:30:54 +0200

    server

--- unboxing-server/app/imports/client/ChallengesInfo.js
+++ unboxing-server/app/imports/client/ChallengesInfo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { css } from 'emotion'
 
-import {Challenges, Servers} from '../collections';
+import {Challenges, Sequences, Servers} from '../collections';
 import ChallengeDetail from './ChallengeDetail';
 
 class ChallengesInfo extends React.Component {
@@ -31,12 +31,14 @@ class ChallengesInfo extends React.Component {
 
   li(c) {
     const server = this.props.servers.find(s => s._id == c.relay_server_id)
-    const server_name = (server ? server.name : "<server does not exist>" )
+    const server_name = (server ? server.name : "<no server>" )
+    const sequence = this.props.sequences.find(s => s._id == c.sequence_id)
+    const sequence_name = (sequence ? sequence.name : "<no sequence>" )
     return (
       <li key={c._id}>
         {this.renderSwitch(c._id)}
         &emsp;
-        {!this.show(c._id) ? <span><b>{c.shorthand}</b> <span style={{paddingLeft: "1ex"}}>{c.name}</span> <small style={{paddingLeft: "1ex"}}>{server_name}</small></span> : null}
+        {!this.show(c._id) ? <span><b>{c.shorthand}</b> <span style={{paddingLeft: "1ex"}}>{c.name}</span> &#x25cf; <span>{sequence_name}</span> &#x25cf; <small>{server_name}</small></span> : null}
         {this.show(c._id) ? <ChallengeDetail challenge={c} /> : null}
       </li>
     );
@@ -87,12 +89,16 @@ export default withTracker(props => {
   const sub1 = Meteor.subscribe('challenges.all');
   const challenges = Challenges.find({}, {sort: {shorthand: 1}}).fetch();
 
-  const sub2 = Meteor.subscribe('servers.all');
+  const sub2 = Meteor.subscribe('sequences.meta');
+  const sequences = Sequences.find().fetch();
+
+  const sub3 = Meteor.subscribe('servers.all');
   const servers = Servers.find().fetch();
 
   return {
     challenges,
+    sequences,
     servers,
-    ready : sub1.ready() && sub2.ready()
+    ready : sub1.ready() && sub2.ready() && sub3.ready()
   };
 })(ChallengesInfo);
