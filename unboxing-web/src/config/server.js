4a8c84602b8fcb493cabf8eaed8e23c7d2f0969e
Sebastian Quack <Sebastian Quack>
2019-06-09 21:07:10 +0200

    point server to live

--- unboxing-web/src/config/server.js
+++ unboxing-web/src/config/server.js
@@ -1,5 +1,5 @@
-//const serverUrl = "http://unboxing.sebquack.perseus.uberspace.de"    
-const serverUrl = "http://localhost:3000"
+const serverUrl = "http://unboxing.sebquack.perseus.uberspace.de"    
+//const serverUrl = "http://localhost:3000"
 const apiPath = "/api/getEverythingWeb.json"
       
 export { serverUrl, apiPath };
